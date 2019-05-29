cb6c11fd02f6d4c54c4c91256518ee7094c21724
Alejandro Ulate <alejandro.ulate@buildto.io>
2019-05-29 13:28:34 -0600

    done

--- app/src/main/java/cr/codingale/trafficsense/TrafficSenseProcessor.java
+++ app/src/main/java/cr/codingale/trafficsense/TrafficSenseProcessor.java
@@ -5,6 +5,8 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfFloat;
 import org.opencv.core.MatOfInt;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
@@ -52,7 +54,8 @@ class TrafficSenseProcessor {
     }
 
     public enum TipoSegmentacion {
-        SIN_PROCESO
+        SIN_PROCESO,
+        SELECCION_CANDIDATOS
     }
 
     public enum TipoReconocimiento {
@@ -288,7 +291,11 @@ class TrafficSenseProcessor {
         }
         switch (tipoSegmentacion) {
             case SIN_PROCESO:
-                salidasegmentacion = salidabinarizacion;
+                salidasegmentacion = entrada;
+                break;
+
+            case SELECCION_CANDIDATOS:
+                seleccionCandidatosCirculos(entrada);
                 break;
         }
         if (mostrarSalida == Salida.SEGMENTACION) {
@@ -519,6 +526,72 @@ class TrafficSenseProcessor {
                 Imgproc.THRESH_BINARY,
                 tamano,
                 -contraste);
+    }
+
+    private void seleccionCandidatosCirculos(Mat entrada) {
+        Imgproc.cvtColor(entrada, gris, Imgproc.COLOR_RGBA2GRAY);
+        Mat binaria = new Mat();
+
+        //Calculo del gradiente morfológico.
+        int contraste = 2;
+        int tamano = 7;
+
+        Mat SE = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new
+                Size(tamano, tamano));
+        Mat gray_dilation = new Mat(); // Result
+        Imgproc.dilate(gris, gray_dilation, SE); // 3x3 dilation
+        Mat dilation_residue = new Mat();
+        Core.subtract(gray_dilation, gris, dilation_residue);
+
+        Imgproc.adaptiveThreshold(
+                dilation_residue,
+                binaria,
+                255,
+                Imgproc.ADAPTIVE_THRESH_MEAN_C,
+                Imgproc.THRESH_BINARY,
+                tamano,
+                -contraste);
+
+
+
+        List<MatOfPoint> blobs = new ArrayList<MatOfPoint>();
+        Mat hierarchy = new Mat();
+        Mat salida = binaria.clone();//Copia porque finContours modifica entrada
+        Imgproc.cvtColor(salida, salida, Imgproc.COLOR_GRAY2RGBA);
+        Imgproc.findContours(binaria, blobs, hierarchy, Imgproc.RETR_CCOMP,
+                Imgproc.CHAIN_APPROX_NONE);
+        int minimumHeight = 30;
+        float maxratio = (float) 0.75;
+        // Seleccionar candidatos a circulos
+        for (int c = 0; c < blobs.size(); c++) {
+            double[] data = hierarchy.get(0, c);
+            int parent = (int) data[3];
+            if (parent < 0) //Contorno exterior: rechazar
+                continue;
+            Rect BB = Imgproc.boundingRect(blobs.get(c));
+            // Comprobar tamaño
+            if (BB.width < minimumHeight || BB.height < minimumHeight)
+                continue;
+            // Comprobar anchura similar a altura
+            float wf = BB.width;
+            float hf = BB.height;
+            float ratio = wf / hf;
+            if (ratio < maxratio || ratio > 1.0 / maxratio)
+                continue;
+            // Comprobar no está cerca del borde
+            if (BB.x < 2 || BB.y < 2)
+                continue;
+            if (entrada.width() - (BB.x + BB.width) < 3 || entrada.height() -
+                    (BB.y + BB.height) < 3)
+                continue;
+            // Aqui cumple todos los criterios. Dibujamos
+            final Point P1 = new Point(BB.x, BB.y);
+            final Point P2 = new Point(BB.x + BB.width, BB.y + BB.height);
+            Imgproc.rectangle(salida, P1, P2, new Scalar(255, 0, 0));
+        } // for
+
+        salidasegmentacion = salida;
+
     }
 }
 
